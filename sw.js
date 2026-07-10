b56562e37e46a66f045e56fd59befe85b4570692
drama815-gif <drama815@gmail.com>
2026-07-10 17:02:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fcc-v0.4.0';
+const CACHE='fcc-v0.4.1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
